ca7d6a2c4171ee92ec7892713aac66a14a7b5496
1QQ6 <771747775@qq.com>
2021-01-14 18:30:51 +0800

    application中注意的事项

--- app/src/main/java/com/wyq/ttmusicapp/utils/MyApplication.kt
+++ app/src/main/java/com/wyq/ttmusicapp/utils/MyApplication.kt
@@ -7,6 +7,9 @@ import com.wyq.ttmusicapp.service.MusicPlayerService
 
 /**
  * Created by Roman on 2021/1/11
+ *
+ * 这里注意AndroidManifest的 android:name=".utils.MyApplication" 配置
+ * 否则onCreate回调不会执行，造成context为空指针异常
  */
 class MyApplication: Application() {
 
